1b384f7dac12fae4bc6703818311879e995e7db0
M Gosschalk <monica.gosschalk@btinternet.com>
2017-09-25 14:36:11 +0100

    added angular google map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,78 @@
 import { Component } from '@angular/core';
 
+// Marker Type
+interface Marker {
+  name?: string,
+  lat: number,
+  lng: number,
+  draggable: boolean
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  // zoom level
+  zoom: number = 10;
+  // Start Position
+  lat: number = 42.858217;
+  lng: number = -70.929990;
+  // Markers
+  markers: Marker[] = [
+    {
+      name: 'Company One',
+      lat: 42.825588,
+      lng: -71.018029,
+      draggable: true
+    },
+    {
+      name: 'Company Two',
+      lat: 42.861864,
+      lng: -70.889071,
+      draggable: true
+    },
+    {
+      name: 'Company Three',
+      lat: 42.828279,
+      lng: -70.930498,
+      draggable: false
+    }
+  ];
+
+  constructor() {}
+
+  clickedMarker(marker: Marker, index: number) {
+    console.log('clicked marker: ', marker.name, ' at index ', index);
+  }
+
+  mapClicked($event: any ) {
+    console.log('map clicked ');
+    let newMarker = {
+      name: 'Untitled',
+      lat: $event.coords.lat,
+      lng: $event.coords.lng,
+      draggable: false
+    }
+    this.markers.push(newMarker);
+  }
+
+  markerDragEnd(marker:any, $event:any) {
+    console.log('dragEnd ', marker, $event);
+    let updMarker = {
+      name: marker.name,
+      lat: parseFloat(marker.lat),
+      lng: parseFloat(marker.lng),
+      draggable: false
+    }
+  }
+
+
+
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AgmCoreModule } from '@agm/core';
+import { AgmSnazzyInfoWindowModule } from '@agm/snazzy-info-window';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCQZkmEZHArtzc8T-PUDCg-JMfcLpUH30M'
+    }),
+    AgmSnazzyInfoWindowModule
   ],
   providers: [],
   bootstrap: [AppComponent]
